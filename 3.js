5d4a37d4978237ee32f4516efa35b3bde0b50c00
Gromova <malyarchyk.17@gmail.com>
2019-02-03 12:00:34 +0200

    Improve third task

--- 3.js
+++ 3.js
@@ -1,18 +1,17 @@
 "use strict"
 
 /**
- * I'm not sure that I understood this task properly
- * and I think there could be a typo in it
+ * I'm not sure that I understood it properly
  * so I just wrote how I think works bind function.
  *
- * Also I used console.log instead of alert.
+ * Also I use console.log instead of alert.
  */
 
 const func1 = function(message) {
-    console.log(message);
+    this(message);
 }
 
-const func2 = func1.bind('Context');
+const func2 = func1.bind(console.log);
 func2('Test'); // alert 'Test'
 
 function myBind(func, context) {
@@ -21,5 +20,5 @@ function myBind(func, context) {
     };
 }
 
-const func3 = myBind(func1, 'Context');
+const func3 = myBind(func1, console.log);
 func3('Test'); // alert 'Test'
